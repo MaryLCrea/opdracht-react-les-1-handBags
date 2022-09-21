b6f7cdf347338653cffbe56bb864297dc29e76bf
MaryLCrea <maryleenen@yahoo.com>
2022-09-21 13:55:45 +0200

    handbags

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+function Button({children,isDisabled,clickHandler}) {
+    return (
+        <button type= "button" disabled={isDisabled} onClick={clickHandler}>
+            {children}
+        </button>
+    )
+}
+export default Button;
